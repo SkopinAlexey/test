ed0279348ce2dbb61e6bcd039b404b49e034274b
Скопин Алексей Сергеевич <a.skopin@univ.priv>
2022-09-16 13:32:30 +0300

    Добавлена 1 после

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ class Hello {
     public static void main(String[] args) {
 	System.out.println("A");
         System.out.println("Hello World!");
-	System.out.printl("B");
+	System.out.println("B");
+	System.out.println("1");
     }
 }
